8c7e0e226554e4e27c3092389eb62183fc2f370d
livjgovob0614 <livjgovob@gmail.com>
2020-02-06 18:17:49 +0900

    0206-2

--- db/jooq_examples/biker_waltz.java
+++ db/jooq_examples/biker_waltz.java
@@ -1,16 +1,16 @@
 
-public CurrentYear computeCurrentYear(int[] totals, int value, String startOfYear) {
+public CurrentYear computeCurrentYear(int[] totals, String startOfYear) {
   
-// 사실 String은 영 아닌데...... jooq 어떤 변수로 이거 받을 수 있는지 낼  확인할게요
-// 미팅 때는 그냥 var로 보여드리고 설명하면 될 것 같아요
+// 사실 String은영 아닌 것 같고  jooq에서 뭘로 이거 받을 수 있는지 낼 확인할게요
+// 미팅 때는 그냥 'var'로 보여드리고 설명해도 될 것 같아요
   String total = sum(MONTYLY_MILAGES.field(MONTHLY_MILAGE_VALUE)).as("total");
-  String idxA = MONTHLY_MILAGES.filed(MILAGES.RECORDED_ON).as("idx");
+  String idxA = MONTHLY_MILAGES.filed(RECORDED_ON).as("idx");
 
   this.database
           .select(idxA,
                   total)
           .from(MONTHLY_MILAGES)
-          .where(MONTHLY_MILAGES.field(RECORDED_ON).isNotNull())
+          .where(MONTHLY_MILAGES.field(MONTHLY_MILAGE_VALUE).isNotNull())
           .and(MONTHLY_MILAGES.filed(RECORDED_ON)
                   .greaterOrEqual(startOfYear);
           .orderBy(MONTHLY_MILAGES.field(BIKES.NAME).asc())
